b2e4f6c956005061208f9ba23eb4a81f5288eff6
Krittaya-Phirason <6531503096@lamduan.mfu.ac.th>
2025-05-29 00:59:08 +0700

    Verified-User/Address

--- smartpayt-client/src/pagesAdmin/AdminService.jsx
+++ smartpayt-client/src/pagesAdmin/AdminService.jsx
@@ -17,6 +17,8 @@ const AdminService = () => {
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
 
     const fetchUsers = async () => {
         try {
@@ -117,8 +119,21 @@ const AdminService = () => {
                             onClick={() => navigate('/admin/debt')}>
                             ข้อมูลผู้ค้างชำระค่าบริการ
                         </li>
-                        <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full"
-                            onClick={() => navigate('/admin/users-verify')}> ยืนยันสถานะที่อยู่ผู้ใช้บริการ </li>
+                        <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                                <div className="flex justify-between items-center">
+                                    <span>ยืนยันสถานะผู้ใช้บริการ</span>
+                                    <svg className={`h-4 w-4 transform transition-transform ${isDropdownOpen ? 'rotate-90' : ''}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                                    </svg>
+                                </div>
+                            </li>
+
+                            {isDropdownOpen && (
+                                <ul className="ml-4">
+                                    <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full" onClick={() => navigate('/admin/verified-user')}>ยืนยันข้อมูลผู้ใช้บริการ</li>
+                                    <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full" onClick={() => navigate('/admin/verified-address')}>ยืนยันข้อมูลครัวเรือน</li>
+                                </ul>
+                            )}
                         <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full"
                             onClick={() => navigate('/admin/bills')}> เพิ่มบิลชำระให้ผู้บริการ </li>
                             <li className="mb-2 p-2 hover:bg-green-900 cursor-pointer rounded px-4 py-3 w-full" onClick={() => navigate('/admin/editwaste')}>ตั้งค่าการเก็บขยะแต่ละประเภท</li>
--- smartpayt-server/routes/adminRoutes.js
+++ smartpayt-server/routes/adminRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 // Public routes
 router.post('/register', adminController.register);
 router.post('/login', adminController.login);
-router.patch('/:lineUserId/address/verify/:addressId',adminController.verifyUserAddress);
-router.post('/users/:lineUserId/add-address', adminController.adduserAsdress);
+router.patch('/:lineUserId/address/verify/:addressId',adminController.verifyAddress);
+router.post('/users/:lineUserId/add-address', adminController.adduserAddress);
 
 
 // Protected routes
@@ -27,8 +27,10 @@ router.get('/debt', adminController.getDebtUsers);
 router.get('/users/:lineUserId/bills', adminController.getBillsByLineUserId);
 
 //Route for AdminVerified page
-router.get('/users-verify', adminController.getUsersWithAddressVerification);  // ดึง users + address_verified
 router.post('/verify-address/:addressId', adminController.verifyAddress);  // ยืนยันที่อยู่
+router.get('/users-verify-user', adminController.getUsersForUserVerification);
+router.get('/users-verify-address', adminController.getUsersWithAddressVerification);
+router.patch('/users/:lineUserId/verify', adminController.verifyUser);
 
 //Route for Admin Manual bill page
 router.post('/bills', verifyToken, adminController.createBill);
@@ -36,5 +38,7 @@ router.get('/waste-pricing', adminController.getWastePricing);
 
 //roure for Edit Waste
 router.post('/waste-pricing', adminController.updateWastePricing);
+
+router.get('/user-address/:lineUserId', adminController.getUserAddress);
 
 module.exports = router;
